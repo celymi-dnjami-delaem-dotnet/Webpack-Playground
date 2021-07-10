b46c2fb84e09c0f13930da9084e00b253c66fc3a
Dmitry Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-07-10 13:47:36 +0300

    Update webpack config to run on dev server app

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import '../index.css';
+import NewComponent from "./NewComponent";
 
 const Main = () => {
     return (
         <div>
             <span className="hello-label">Hello world!</span>
+            <NewComponent />
         </div>
     )
 };
---
+++ src/components/NewComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewComponent = () => {
+    return (<span>New component!</span>);
+}
+
+export default NewComponent;
