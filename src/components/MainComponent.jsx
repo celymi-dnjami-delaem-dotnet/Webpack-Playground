ad44166d2ff1f1d894f98ce38d9d5ec1e7edbb95
Dmitry Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-07-10 13:26:28 +0300

    Add base components for app launch

---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../index.css';
+
+const Main = () => {
+    return (
+        <div>
+            <span className="hello-label">Hello world!</span>
+        </div>
+    )
+};
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ReactDom from 'react-dom';
+import Main from "./components/MainComponent";
+
+ReactDom.render(<Main />, document.getElementById('root'));
